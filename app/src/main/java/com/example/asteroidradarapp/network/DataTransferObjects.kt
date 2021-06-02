bf57fa474a194b6aa7d5aec5c983fbba9e653463
SILVIA-CECIN <silvia.cecin@hotmail.com>
2021-06-02 19:45:47 +0100

    Create DataTransferObjects

--- app/src/main/java/com/example/asteroidradarapp/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/asteroidradarapp/network/DataTransferObjects.kt
@@ -1,34 +1,34 @@
-//package com.example.asteroidradarapp.network
-//
-//import com.example.asteroidradarapp.database.DatabaseAsteroid
-//import com.squareup.moshi.JsonClass
-//
-//@JsonClass(generateAdapter = true)
-//data class NetworkAsteroidContainer(val asteroids: List<NetworkAsteroid>)
-//
-//@JsonClass(generateAdapter = true)
-//data class NetworkAsteroid(
-//    val id: Long,
-//    val codename: String,
-//    val closeApproachDate: String,
-//    val absoluteMagnitude: Double,
-//    val estimatedDiameter: Double,
-//    val relativeVelocity: Double,
-//    val distanceFromEarth: Double,
-//    val isPotentiallyHazardous: Boolean
-//)
-//
-//fun NetworkAsteroidContainer.asDatabaseModel(): Array<DatabaseAsteroid> {
-//    return asteroids.map {
-//        DatabaseAsteroid(
-//            id = it.id,
-//            codename = it.codename,
-//            closeApproachDate = it.closeApproachDate,
-//            absoluteMagnitude = it.absoluteMagnitude,
-//            estimatedDiameter = it.estimatedDiameter,
-//            relativeVelocity = it.relativeVelocity,
-//            distanceFromEarth = it.distanceFromEarth,
-//            isPotentiallyHazardous = it.isPotentiallyHazardous
-//        )
-//    }.toTypedArray()
-//}
+package com.example.asteroidradarapp.network
+
+import com.example.asteroidradarapp.database.DatabaseAsteroid
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class NetworkAsteroidContainer(val asteroids: List<NetworkAsteroid>)
+
+@JsonClass(generateAdapter = true)
+data class NetworkAsteroid(
+    val id: Long,
+    val codename: String,
+    val closeApproachDate: String,
+    val absoluteMagnitude: Double,
+    val estimatedDiameter: Double,
+    val relativeVelocity: Double,
+    val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+)
+
+fun NetworkAsteroidContainer.asDatabaseModel(): Array<DatabaseAsteroid> {
+    return asteroids.map {
+        DatabaseAsteroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = it.closeApproachDate,
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyHazardous = it.isPotentiallyHazardous
+        )
+    }.toTypedArray()
+}
